6b3c866cef609c63a40cec9df886ac9ca77ad402
Remmery Julien <julien.remmery@student.vinci.be>
2025-01-29 02:24:27 +0100

    Add example code for JWT auth middleware and login logic with token creation

--- server.c
+++ server.c
@@ -3,7 +3,13 @@
 #define JUTILS_IMPLEMENTATION
 #define HTTP_IMPLEMENTATION
 #define JACON_IMPLEMENTATION
+#define SHA_IMPLEMETNATION
+#define HMAC_IMPLEMENTATION
+#define BASE64_IMPLEMENTATION
+#define TOKI_IMPLEMENTATION
 #include "webserver.h"
+#include "toki.h"
+#include <time.h>
 
 int
 route_get_root(Route* route, Http_Request* req, Http_Response* res)
@@ -39,33 +45,92 @@ route_get_favicon(Route* route, Http_Request* req, Http_Response* res)
     return 0;
 }
 
+bool
+validate_credentials(const char* login, const char* password)
+{
+    return login != NULL 
+        && password != NULL 
+        && strlen(login) != 0 
+        && strlen(password) != 0;
+}
+
+char*
+create_token(const char* login)
+{
+    Toki_Token token = {0};
+    Toki_token_init(&token, TOKI_ALG_HS256);
+    Jacon_Node iss = Jacon_string_prop("iss", "Conrad");
+    Jacon_Node exp = Jacon_int_prop("exp", (int)time(NULL));
+    Jacon_Node login_node = Jacon_string_prop("login", login);
+    Toki_add_claim(&token, &iss);
+    Toki_add_claim(&token, &exp);
+    Toki_add_claim(&token, &login_node);
+
+    char* signed_token;
+    Toki_sign_token(&token, &signed_token);
+    return signed_token;
+}
+
 int
-route_get_users(Route* route, Http_Request* req, Http_Response* res)
+route_post_login(Route* route, Http_Request* req, Http_Response* res)
 {
     (void)route;
-    res->status = HTTP_STATUS_NOT_IMPLEMENTED;
-    Ws_send_response(req->client_fd, res);
+    int ret;
+    char* login;
+    ret = Jacon_get_string_by_name(&req->body, "login", &login);
+    char* password;
+    Jacon_get_string_by_name(&req->body, "password", &password);
+
+    if (!validate_credentials(login, password)) {
+        res->status = HTTP_STATUS_BAD_REQUEST;
+        return Ws_send_response(req->client_fd, res);
+    }
+
+    char* signed_token = create_token(login);
+
+    Jacon_Node json_object = Jacon_object();
+    Jacon_Node token_node = Jacon_string_prop("token", signed_token);
+    Jacon_append_child(&json_object, &token_node);
+
+    res->status = HTTP_STATUS_OK;
+    Jacon_serialize(&json_object, &res->content);
+
+    free(login);
+    free(password);
+    free(signed_token);
+
+    return Ws_send_response_with_content(req->client_fd, res, HTTP_CONTENTTYPE_JSON);
+}
+
+int
+route_get_dashboard(Route* route, Http_Request* req, Http_Response* res)
+{
+    (void)route;
+    res->status = HTTP_STATUS_OK;
+    Ws_send_response_with_file(req->client_fd, res, "static/dashboard.html");
     return 0;
 }
 
 int
-authorized(Route* route, Http_Request* req, Http_Response* res)
+authorize(Route* route, Http_Request* req, Http_Response* res)
 {
     (void)route;
-    (void)req;
-    (void)res;
-    return 0;
-}
+    char* auth_header = hm_get(&req->headers, "authorization");
+    if (auth_header == NULL) {
+        res->status = HTTP_STATUS_UNAUTHORIZED;
+        Ws_send_response(req->client_fd, res);
+        return 1;
+    }
 
-int
-unauthorized(Route* route, Http_Request* req, Http_Response* res)
-{
-    (void)route;
-    (void)req;
-    (void)res;
-    res->status = HTTP_STATUS_UNAUTHORIZED;
-    Ws_send_response(req->client_fd, res);
-    return 1;
+    if (!Toki_verify_token(auth_header)) {
+        res->status = HTTP_STATUS_UNAUTHORIZED;
+        Ws_send_response(req->client_fd, res);
+        return 1;
+    }
+
+    // Additional work on token data
+    
+    return 0;
 }
 
 Ws_Router
@@ -79,7 +144,8 @@ setup_router()
     Ws_router_handle(&router, "/index.js", HTTP_METHOD_GET, route_get_root_js, NULL);
     Ws_router_handle(&router, "/index.css", HTTP_METHOD_GET, route_get_root_css, NULL);
     Ws_router_handle(&router, "/favicon.ico", HTTP_METHOD_GET, route_get_favicon, NULL);
-    Ws_router_handle(&router, "/users", HTTP_METHOD_GET, route_get_users, authorized);
+    Ws_router_handle(&router, "/dashboard", HTTP_METHOD_GET, route_get_dashboard, authorize);
+    Ws_router_handle(&router, "/api/login", HTTP_METHOD_POST, route_post_login, NULL);
     return router;
 }
 
